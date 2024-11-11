e05a27fd25cec4590140dc37c5dd6b0a7dee9621
Thiago Duarte <me@tduarte.co>
2024-11-11 11:17:21 -0800

    Hyprland conf updates
    Add ability to see a list of keybinds (script by ML4W) and (try) fix some color and tearing issues.

---
+++ .config/hypr/scripts/rofi_keybinds.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#  _              _     _           _ _
+# | | _____ _   _| |__ (_)_ __   __| (_)_ __   __ _ ___
+# | |/ / _ \ | | | '_ \| | '_ \ / _` | | '_ \ / _` / __|
+# |   <  __/ |_| | |_) | | | | | (_| | | | | | (_| \__ \
+# |_|\_\___|\__, |_.__/|_|_| |_|\__,_|_|_| |_|\__, |___/
+#           |___/                             |___/
+
+# -----------------------------------------------------
+# Path to keybindings config file
+# -----------------------------------------------------
+config_file="$HOME/.config/hypr/conf/keybinding.conf"
+echo "Reading from: $config_file"
+
+keybinds=""
+
+# Detect Start String
+while read -r line
+do
+    if [[ "$line" == "bind"* ]]; then
+
+        line="$(echo "$line" | sed 's/$mainMod/SUPER/g')"
+        if [[ "$line" == *"XF86"* ]]; then
+            continue
+        fi
+        line="$(echo "$line" | sed 's/bind = //g')"
+        line="$(echo "$line" | sed 's/bindm = //g')"
+        line="$(echo "$line" | sed 's/bindr = //g')"
+
+        IFS='#'
+        read -a strarr <<<"$line"
+        kb_str=${strarr[0]}
+        cm_str=${strarr[1]}
+
+        IFS=','
+        read -a kbarr <<<"$kb_str"
+
+        item="${kbarr[0]}  + ${kbarr[1]}"$'\r'"${cm_str:1}"
+        keybinds=$keybinds$item$'\n'
+    fi
+done < "$config_file"
+
+sleep 0.2
+rofi -dmenu -i -markup -eh 2 -replace -p "Keybinds" <<< "$keybinds"
